86979887a46f4dea9b9e8b33bceed221d9923d40
MasonCheng0312 <mason9003122001@gmail.com>
2024-04-01 14:42:57 +0800

    output the result which didn't match the table

--- addInfo_to_annotation.py
+++ addInfo_to_annotation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from Bio import SeqIO
 
 input_path = "/home/cosbi2/py_project/input/JL_info.csv"
 jlan_data = pd.read_csv(input_path,sep="\t",usecols=['qseqID', 'pident', 'E value', 'bitscore'])
@@ -14,9 +15,21 @@ jlan_col = ['Library Name', 'Gene ID', 'Gene Location', 'Gene Expression',
 jlan_table = pd.read_csv(origin_table_path, sep=",", index_col=None,header=None)
 jlan_table.columns = jlan_col
 jlan_table.drop(columns=['Score', 'Expect value', 'Identities'],inplace=True)
-print(jlan_table)
+# print(jlan_table)
 
 result = pd.merge(jlan_table,jlan_data,on="Gene ID",how="left")
-print(result)
+# print(result)
 result = result.reindex(columns= jlan_col)
-print(result)
+result.to_csv("/home/cosbi2/py_project/tools/output/JL_result.csv",index=False)
+data_to_handle = result[result["Expect value"].isna()]
+name_list = data_to_handle["Gene ID"].tolist()
+# print(name_list)
+input_pep = "/home/cosbi2/py_project/input/Censifolium_pep.fa"
+outputPATH = "/home/cosbi2/py_project/tools/output/mismatch_pep.fa"
+with open(outputPATH,"w") as output:
+	input_content = SeqIO.parse(input_pep,"fasta")
+	for record in input_content:
+		if record.name in name_list:
+			output.write(">" + record.name + "\n")
+			output.write(f'{record.seq}'+ "\n")
+		else:continue
